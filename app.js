cc267b69abf8a919ebc7e69508f6807e8fbb5219
Yahya Hussein <yahyahussein91@gmail.com>
2017-05-28 15:04:25 -0700

    started on dom

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ var scores, roundScore, activePlayer, dice;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 0;
+activePlayer = 1;
 
 dice = Math.floor(Math.random() * 6) + 1;
+
+//DOM Manipulation
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</>'
+document.querySelector('#current-' + activePlayer).textContent = dice;
