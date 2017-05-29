a2b56c0924eec442bb1310b5da0d61e343db96a2
Yahya Hussein <yahyahussein91@gmail.com>
2017-05-29 14:38:35 -0700

    removing winner class

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
 	//Check if player won the game
-	if(scores[activePlayer] >= 20){
+	if(scores[activePlayer] >= 50){
 		document.querySelector('#name-' + activePlayer).textContent = 'Winner';
 		document.querySelector('.dice').style.display = 'none';
 		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
@@ -87,6 +87,8 @@ function init(){
 	document.getElementById('current-1').textContent = '0';
 	document.getElementById('name-0').textContent = 'Players-1';
 	document.getElementById('name-1').textContent = 'Players-1';
+	document.querySelector('.player-' + activePlayer + '-panel').classList.remove('winner');
+
 
 
 }
