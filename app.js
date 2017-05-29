9f760d3788f628eac4d86ef8422383ec78cd12b6
Yahya Hussein <yahyahussein91@gmail.com>
2017-05-29 14:40:35 -0700

    added active class to player one at start of game

--- app.js
+++ app.js
@@ -91,6 +91,7 @@ function init(){
 	document.querySelector('.player-1').classList.remove('winner');
 	document.querySelector('.player-0-panel').classList.remove('active');
 	document.querySelector('.player-1-panel').classList.remove('active');
+	document.querySelector('.player-0-panel').classList.add('active');
 
 
 
