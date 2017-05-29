51225a7df8b30f1fdb9e6c36c72ed8500b6e3c62
Yahya Hussein <yahyahussein91@gmail.com>
2017-05-29 14:28:11 -0700

    change background win player wins

--- app.js
+++ app.js
@@ -58,6 +58,8 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 	if(scores[activePlayer] >= 20){
 		document.querySelector('#name-' + activePlayer).textContent = 'Winner';
 		document.querySelector('.dice').style.display = 'none';
+		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 	}else{
 		nextPlayer();
 	}
